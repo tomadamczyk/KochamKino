d07ff62456a826d40ee45eca96128ee165a116ed
Tomasz Adamczyk <tomasz_adamczyk@outlook.com>
2020-05-16 13:28:04 +0200

    deleted shit XD

--- src/main/java/com/example/kochamkino/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.kochamkino.service;
-
-import com.example.kochamkino.models.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
